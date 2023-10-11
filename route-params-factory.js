3f3b9b30ebfb13e4762a185de2510acc006a23ff
3phase <pbozhinow@gmail.com>
2023-10-11 14:31:52 +0200

    Removes switch

--- route-params-factory.js
+++ route-params-factory.js
@@ -11,17 +11,6 @@ class RouteParamsFactory {
                 return req;
             case route_paramtypes_enum_1.RouteParamtypes.RESPONSE:
                 return res;
-            case route_paramtypes_enum_1.RouteParamtypes.BODY:
-                return data && req.body ? req.body[data] : req.body;
-            case route_paramtypes_enum_1.RouteParamtypes.PARAM:
-                return data ? req.params[data] : req.params;
-            case route_paramtypes_enum_1.RouteParamtypes.HOST:
-                const hosts = req.hosts || {};
-                return data ? hosts[data] : hosts;
-            case route_paramtypes_enum_1.RouteParamtypes.QUERY:
-                return data ? req.query[data] : req.query;
-            case route_paramtypes_enum_1.RouteParamtypes.HEADERS:
-                return data ? req.headers[data.toLowerCase()] : req.headers;
             case route_paramtypes_enum_1.RouteParamtypes.SESSION:
                 return req.session;
             case route_paramtypes_enum_1.RouteParamtypes.FILE:
