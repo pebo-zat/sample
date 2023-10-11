33316116aba3a22c1eb90df65589e6e0a23406bb
3phase <pbozhinow@gmail.com>
2023-10-11 14:04:30 +0200

    Changes route path factory

--- route-path-factory.d.ts
+++ route-path-factory.d.ts
@@ -5,6 +5,9 @@ export declare class RoutePathFactory {
     private readonly applicationConfig;
     constructor(applicationConfig: ApplicationConfig);
     create(metadata: RoutePathMetadata, requestMethod?: RequestMethod): string[];
+    create(metadata: RoutePathMetadata, requestMethod?: RequestMethod): string[];
+    create(metadata: RoutePathMetadata, requestMethod?: RequestMethod): string[];
+    create(metadata: RoutePathMetadata, requestMethod?: RequestMethod): string[];
     getVersion(metadata: RoutePathMetadata): import("@nestjs/common/interfaces").VersionValue;
     getVersionPrefix(versioningOptions: VersioningOptions): string;
     appendToAllIfDefined(paths: string[], fragmentToAppend: string | string[] | undefined): string[];
